156ad5d82b4e340fd2123cac8267f07680f8efd4
noobflick <rishavsanjan0@gmail.com>
2026-09-09 19:46:01 +0530

    added a load balancer, a target group, 2 target group attachment and a load balancer listener

--- main.tf
+++ main.tf
@@ -3,16 +3,16 @@ resource "aws_vpc" "myvpc" {
 }
 
 resource "aws_subnet" "sub1" {
-  cidr_block = "10.0.0.0/24"
-  availability_zone = "us-east-1a"
-  vpc_id = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
+  vpc_id                  = aws_vpc.myvpc.id
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "sub2" {
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1b"
-  vpc_id = aws_vpc.myvpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1b"
+  vpc_id                  = aws_vpc.myvpc.id
   map_public_ip_on_launch = true
 }
 
@@ -30,37 +30,37 @@ resource "aws_route_table" "RT" {
 }
 
 resource "aws_route_table_association" "rta1" {
-  subnet_id = aws_subnet.sub1.id 
-  route_table_id =  aws_route_table.RT.id
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.RT.id
 }
 
 resource "aws_route_table_association" "rta2" {
-  subnet_id = aws_subnet.sub2.id 
-  route_table_id =  aws_route_table.RT.id
+  subnet_id      = aws_subnet.sub2.id
+  route_table_id = aws_route_table.RT.id
 }
 
 resource "aws_security_group" "sg" {
-  name = "security-group-using-terraform"
-  vpc_id = aws_vpc.myvpc.id 
-  
+  name   = "security-group-using-terraform"
+  vpc_id = aws_vpc.myvpc.id
+
   ingress {
     description = "HTTP from VPC"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "SSH"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -71,18 +71,65 @@ resource "aws_s3_bucket" "s3" {
 
 
 resource "aws_instance" "webserver1" {
-  ami = "ami-0b6d9d3d33ba97d99"
-  instance_type = "t2.micro"
+  ami                    = "ami-0b6d9d3d33ba97d99"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
-  subnet_id = aws_subnet.sub1.id
-  user_data = base64encode(file("ws1data.sh"))
+  subnet_id              = aws_subnet.sub1.id
+  user_data              = base64encode(file("ws1data.sh"))
 }
 
 resource "aws_instance" "webserver2" {
-  ami = "ami-0b6d9d3d33ba97d99"
-  instance_type = "t2.micro"
+  ami                    = "ami-0b6d9d3d33ba97d99"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
-  subnet_id = aws_subnet.sub2.id
-  user_data = base64encode(file("ws2data.sh"))
+  subnet_id              = aws_subnet.sub2.id
+  user_data              = base64encode(file("ws2data.sh"))
+}
+
+resource "aws_lb" "mylb" {
+  name               = "lb-using-terraform"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg.id]
+  subnets            = [aws_subnet.sub1.id, aws_subnet.sub2.id]
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "target-1"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.myvpc.id
+
+  health_check {
+    path = "/"
+    port = "traffic-port"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "attach1" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.webserver1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "attach2" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.webserver2.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "listner" {
+  load_balancer_arn = aws_lb.mylb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.tg.arn
+    type             = "forward"
+  }
+}
+
+output "loadbalancerdns" {
+  value = aws_lb.mylb.dns_name
 }
 
